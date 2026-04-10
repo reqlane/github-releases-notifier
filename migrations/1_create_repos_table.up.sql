501ed61152e0082b775e3c5f5f09054bff1d1551
reqlane <107763485+reqlane@users.noreply.github.com>
2026-04-10 17:49:14 +0000

    Schema migrations changed

---
+++ migrations/1_create_repos_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS repos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    repo VARCHAR(255) NOT NULL UNIQUE,
+    last_seen_tag VARCHAR(255) NOT NULL,
+    INDEX idx_repo (repo)
+);
--- migrations/2_create_subscriptions_table.up.sql
+++ migrations/2_create_subscriptions_table.up.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS subscriptions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    repo VARCHAR(255) NOT NULL,
+    repo_id INT NOT NULL,
     confirmed BOOLEAN NOT NULL DEFAULT false,
-    last_seen_tag VARCHAR(255) NOT NULL DEFAULT '',
     confirm_token VARCHAR(255),
     unsubscribe_token VARCHAR(255) NOT NULL,
-    UNIQUE KEY unique_email_repo (email, repo)
+    UNIQUE KEY idx_email_repo (email, repo_id),
+    INDEX idx_confirm_token (confirm_token),
+    INDEX idx_unsubscribe_token (unsubscribe_token),
+    FOREIGN KEY (repo_id) REFERENCES repos(id)
 );
